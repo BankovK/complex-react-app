787e638c399ab5311054d48f97e15c0c485ace44
unknown <KBankou@gomel.iba.by>
2020-12-18 02:24:57 +0300

    section13-unit74-75

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,8 +4,14 @@ import HeaderLoggedOut from "./HeaderLoggedOut"
 import HeaderLoggedIn from "./HeaderLoggedIn"
 import StateContext from "../StateContext"
 
-function Header() {
+function Header(props) {
   const appState = useContext(StateContext)
+  const headerContent = appState.loggedIn ? (
+    <HeaderLoggedIn />
+  ) : (
+    <HeaderLoggedOut />
+  )
+
   return (
     <header className="header-bar bg-primary mb-3">
       <div className="container d-flex flex-column flex-md-row align-items-center p-3">
@@ -15,7 +21,7 @@ function Header() {
             ComplexApp{" "}
           </Link>
         </h4>
-        {appState.loggedIn ? <HeaderLoggedIn /> : <HeaderLoggedOut />}
+        {!props.staticEmpty ? headerContent : ""}
       </div>
     </header>
   )
